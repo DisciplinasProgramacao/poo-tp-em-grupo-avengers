69b2d5d7ad35d2e77a56b5dfa6aed75481fe7924
Gabriel Cotoschi <104593354+Cotoschi@users.noreply.github.com>
2023-10-05 13:30:00 -0300

    Relatório de rota
    Adicionado código para a geração de relatório dentro de rota

---
+++ codigo/Rota.java
@@ -0,0 +1,40 @@
+
+
+public class Rota {
+    private double quilometragem;
+    private String data;
+    private int quantCarro;
+    
+    public Rota(double quilometragem, String data) {
+        this.quilometragem = quilometragem;
+        quantCarro++;
+        this.data = data;
+    }
+    
+    public String relatorio() {
+        // Gerar um relatório da rota aqui
+        //Mostar a quantidade de carros e o dia do relatorio
+    	StringBuilder relat = new StringBuilder("Relatório de rota");
+    	relat.append(quantCarros + "\n" + data);
+        return relat.toString();
+    }
+
+
+    //Get and Set
+
+    public double getQuilometragem() {
+        return quilometragem;
+    }
+
+    public void setQuilometragem(double quilometragem) {
+        this.quilometragem = quilometragem;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
