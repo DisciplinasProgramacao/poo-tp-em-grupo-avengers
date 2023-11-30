a26ad1fd378d4afb0ffe31814083ef5a5027a722
Gabriel Cotoschi <104593354+Cotoschi@users.noreply.github.com>
2023-11-30 02:35:07 -0300

    Update Caminhao.java
    n tinha colado tudo, perdão

--- codigo/frota/src/Caminhao.java
+++ codigo/frota/src/Caminhao.java
@@ -37,3 +37,144 @@ public class Caminhao {
         }
         return false;
     }
+    
+    public double autonomiaAtual() {
+        return tanqueAtual * CONSUMO;
+    }
+    
+    public double autonomiaMaxima() {
+        return tanqueMax * CONSUMO;
+    }
+    
+    public double abastecer(double litros) {
+        return tanque.abastecer(litros);
+    }
+    
+    public double kmMes() {
+        double quilometragemMes = 0;
+        for (Rota rota : rotas) {
+            quilometragemMes += rota.getQuilometragem();
+        }
+        return quilometragemMes;
+    }
+    
+    public double kmTotal() {
+        double quilometragemTotal = 0;
+        for (Rota rota : rotas) {
+            quilometragemTotal += rota.getQuilometragem();
+        }
+        return quilometragemTotal;
+    }
+    
+    public void percorrerRota(Rota rota) {
+    	//Confere antes se o km da rota + o km rodado desde a ultima manutencao nao ultrapassa o valor exigido para manutencoes
+    	if(kmManutencao + rota.getQuilometragem() < manutencaoProg) {   
+        if (autonomiaMaxima() >= rota.getQuilometragem()) {
+            tanqueAtual -= rota.getQuilometragem() / CONSUMO;
+            rotas.add(rota);
+            quantRotas++;
+            //Calcula e soma o valor da despesa de acorda com a KM da rota
+            despesa += ( rota.getQuilometragem() / CONSUMO )* valorDiesel;
+        }
+    	}else {
+        //Caso o valor ultrapasse o exigido a manutenção será ordenada
+        	manutencao(); 
+        }
+    }
+    
+    public void manutencao() {
+    	fezManutencao = kmTotal();
+    	kmManutencao = 0;
+    	//Adicionar 100 reais ao custo da despesa a cada manutencao
+    	despesa += 100;
+    }
+    
+    public void consumoTotal() {
+    	if (tipoTanque == "diesel") {
+    		consumoTotal = (kmTotal() / CONSUMO) * valorDiesel;
+    	}
+    }
+
+   //Gets and Setters
+   
+    public Object getPlaca() {
+        return placa;
+    }
+
+    public static int getMaxRotas() {
+        return MAX_ROTAS;
+    }
+
+    public static double getConsumo() {
+        return CONSUMO;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public List<Rota> getRotas() {
+        return rotas;
+    }
+
+    public void setRotas(List<Rota> rotas) {
+        this.rotas = rotas;
+    }
+
+    public int getQuantRotas() {
+        return quantRotas;
+    }
+
+    public void setQuantRotas(int quantRotas) {
+        this.quantRotas = quantRotas;
+    }
+
+    public double getTanqueAtual() {
+        return tanqueAtual;
+    }
+
+    public void setTanqueAtual(double tanqueAtual) {
+        this.tanqueAtual = tanqueAtual;
+    }
+
+    public double getTanqueMax() {
+        return tanqueMax;
+    }
+
+    public void setTanqueMax() {
+        this.tanqueMax = 250;	
+    }
+
+    public double getTotalReabastecido() {
+        return totalReabastecido;
+    }
+
+    public void setTotalReabastecido(double totalReabastecido) {
+        this.totalReabastecido = totalReabastecido;
+    }
+
+    public Tanque getTanque() {
+        return tanque;
+    }
+
+    public void setTanque(Tanque tanque) {
+        this.tanque = tanque;
+    }
+    
+    public void getConsumo() {
+    	return consumoTotal;
+    }
+    //Retorna a ultima vez em que o veiculo sofreu uma manutencao
+    public double getUltimaManutencao() {
+    	return fezManutencao;
+    }
+    //Retorna a quantidade de KMs até a próxima manutencao obrigatória
+    public double getProximaManutencao() {
+    	return manutencaoProg - kmManutencao;
+    }
+    
+    //Retorna a despesa total do veículo (manutencoes e kilometragem andada)
+    public double getDespesa() {
+    	return despesa;
+    }
+}
