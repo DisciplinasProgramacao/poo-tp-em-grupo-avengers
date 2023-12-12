14c69c27ec3bffd565d257764a18de35520b97e7
Julia Mello <104536648+julialeal03@users.noreply.github.com>
2023-12-12 17:57:34 -0300

    Update Carro

--- codigo/frota/src/Carro.java
+++ codigo/frota/src/Carro.java
@@ -1,71 +1,94 @@
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.Calendar;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Carro extends Veiculo {
-    private String categoria;
-    private int portas;
-    private double totalReabastecido;
-    private int quantRotas;
-    private Rota[] rotas;
-    private double consumo;
-    private int MAX_ROTAS;
-    private double preçoLitro;
-
-    public Carro(String placa, double modelo, double consumo, String categoria, int portas) {
-        super(placa, modelo, consumo);
-        this.categoria = categoria;
-        this.portas = portas;
-        this.totalReabastecido = 0.0;
+    private static final int MAX_ROTAS = 30;
+    
+	private double totalReabastecido;
+	private int quantRotas;
+	private Rota[] rotas;	
+    private String placa;
+	private Tanque tanque;
+	private Manutencao manutencao = new Manutencao();
+    
+    public Carro(double consumo, String placa) {
+        super(placa, consumo, consumo);
+		tanque = new Tanque(50,7,"alcool");
+        this.placa = placa;
+        this.rotas = new ArrayList<>();
         this.quantRotas = 0;
-        this.rotas = new Rota[MAX_ROTAS];
+    }
+    
+    public boolean addRota(Rota rota) {
+        if (quantRotas < MAX_ROTAS && autonomiaMaxima() >= rota.getQuilometragem()) {
+            rotas.add(rota);
+            quantRotas++;
+            return true;
+        }
+        return false;
+    }
+    
+    public double autonomiaAtual() {
+        return tanque.getCapacidadeAtual() * tanque.getConsumo();
+    }
+    
+    public double autonomiaMaxima() {
+        return tanque.getCapacidadeMax() * tanque.getConsumo();
+    }
+    
+    public double abastecer(double litros) {
+        return tanque.abastecer(litros);
+    }
+    
+    public double kmNoMes() {
+        double quilometragemMes = 0;
+        for (Rota rota : rotas) {
+            quilometragemMes += rota.getQuilometragem();
+        }
+        return quilometragemMes;
+    }
+    
+    public double kmTotal() {
+        double quilometragemTotal = 0;
+        for (Rota rota : rotas) {
+            quilometragemTotal += rota.getQuilometragem();
+        }
+        return quilometragemTotal;
+    }
+    
+    public void percorrerRota(Rota rota) { 
+            tanqueAtual -= rota.getQuilometragem() / tanque.getConsumo();
+            rotas.add(rota);
+            quantRotas++;
+        }
+    }
+	
+	public void fazerManutencao(){
+			manutencao.fazerManutencao(kmTotal())
+    }
+   //Gets and Setters
+   
+    public Object getPlaca() {
+        return placa;
     }
 
-    @Override
-    public String toString() {
-        return "Carro{" +
-                "placa='" + placa + '\'' +
-                ", consumo=" + consumo +
-                ", categoria='" + categoria + '\'' +
-                ", portas=" + portas +
-                ", totalReabastecido=" + totalReabastecido +
-                ", quantRotas=" + quantRotas +
-                ", rotas=" + Arrays.toString(rotas) +
-                '}';
+    public static int getMaxRotas() {
+        return MAX_ROTAS;
     }
 
-    //Métodos Getters e Setters
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
+    public static double getConsumo() {
+        return Tanque.getConsumo();
     }
 
-    public String getCategoria() {
-        return categoria;
+    public void setPlaca(String placa) {
+        this.placa = placa;
     }
 
-    public void setPortas(int portas) {
-        this.portas = portas;
+    public List<Rota> getRotas() {
+        return rotas;
     }
 
-    public int getPortas() {
-        return portas;
-    }
-
-    public void setTotalReabastecido(double totalReabastecido) {
-        this.totalReabastecido = totalReabastecido;
-    }
-
-    public double getTotalReabastecido() {
-        return totalReabastecido;
-    }
-
-    public void setQuantRotas(int quantRotas) {
-        this.quantRotas = quantRotas;
-    }
-
-    public void setRotas(Rota[] rotas) {
+    public void setRotas(List<Rota> rotas) {
         this.rotas = rotas;
     }
 
@@ -73,86 +96,11 @@ public class Carro extends Veiculo {
         return quantRotas;
     }
 
-    public List<Rota> getRotas() {
-        return rotas;
+    public void setQuantRotas(int quantRotas) {
+        this.quantRotas = quantRotas;
     }
 
-    public boolean addRota(Rota rota) {
-        if (quantRotas < MAX_ROTAS) {
-            rotas[quantRotas++] = rota;
-            return true;
-        }
-        return false;
-    }
-
-    public void percorrerRota(Rota rota) {
-        double distancia = rota.getQuilometragem();
-        double litros = distancia / consumo;
-        totalReabastecido += litros;
-        quantRotas++;
-    }
-
-    public double KmTotal() {
-        double kmTotal = 0.0;
-        for (int i = 0; i < quantRotas; i++) {
-            kmTotal += rotas[i].getQuilometragem();
-        }
-        return kmTotal;
-    }
-
-    /**
-     * Calcula a quilometragem rodada pelo carro naquele mes de acordo com a data atual do sistema 
-     * @return valor da quilometragem por mês
-     */
-    public double KmNoMes() {
-    if (quantRotas == 0) {
-        return 0.0; // Não há rotas registradas, então não há quilômetros no mês
-    }
-    LocalDate dataAtual = LocalDate.now();
-    double kmNoMes = 0.0;
-    for (int i = 0; i < quantRotas; i++) {
-        LocalDate dataRota = LocalDate.parse(rotas[i].getData(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-
-        if (dataRota.getMonth().equals(dataAtual.getMonth()) && dataRota.getYear() == dataAtual.getYear()) {
-            kmNoMes += rotas[i].getQuilometragem();
-        }
-    }
-    return kmNoMes;
-}
-    /**
-     * Retorna o valor de abastecimento do carro
-     */
-    public double abastecer(double litros) {
-        double valor = litros * preçoLitro;
-        totalReabastecido += litros;
-        return valor;
-    }
-
-    /**
-     * Método que calcula o valor da autonomia atual do carro
-     * @return valor 
-     */
-    public double getAutonomiaAtual() {
-        return KmTotal() / totalReabastecido;
-    }
-
-    /**
-     * Calcula autonomia máxima de consumo do carro
-     * @return valor 
-     */
-    public double getAutonomiaMaxima() {
-        return 100.0 / consumo;
-    }
-
-    public void fazerManutencao(double valor) {
-        despesaTotal += valor;
-    }
-
-    /**
-     * Método de rotorno do valor de despesa total do carro
-     * @return valor da despesa total
-     */
-    public double getDespesaTotal() {
-        return despesaTotal;
+    public double getTotalReabastecido() {
+        return tanque.geTotalReabastecido();
     }
 }
